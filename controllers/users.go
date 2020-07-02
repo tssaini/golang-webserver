b27381115280fdaa37254cd2c51039b907706b5b
Taranpreet Saini <taranpreet.s16@gmail.com>
2020-07-02 02:04:39 -0400

    added post and get method to get and list users

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"encoding/json"
+	"strconv"
+	"github.com/tssaini/golang-webserver/models"
 	"regexp"
 	"net/http"
 )
@@ -11,7 +14,71 @@ type userController struct{
 }
 
 func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Hello from the User controller!"))
+	if r.URL.Path == "/users"{
+		switch r.Method {
+			case http.MethodGet:
+				uc.getAll(w, r)
+			case http.MethodPost:
+				uc.post(w,r)
+			default:
+				w.WriteHeader(http.StatusNotImplemented)
+		}
+
+	}else{
+		matches := uc.userIDPattern.FindStringSubmatch(r.URL.Path)
+		if len(matches) == 0{
+			w.WriteHeader(http.StatusNotFound)
+		}
+		id, err := strconv.Atoi(matches[1])
+		if err != nil{
+			w.WriteHeader(http.StatusNotFound)
+		}
+		switch r.Method {
+			case http.MethodGet:
+				uc.get(id, w)
+			default:
+				w.WriteHeader(http.StatusNotImplemented)
+		}
+	}
+}
+
+func (uc *userController) getAll(w http.ResponseWriter, r *http.Request){
+	encodeResponseAsJSON(models.GetUsers(), w)
+}
+
+func (uc *userController) get(id int, w http.ResponseWriter){
+	u, err := models.GetUserByID(id)
+	if err != nil{
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	encodeResponseAsJSON(u, w)
+}
+
+func (uc *userController) post(w http.ResponseWriter, r *http.Request){
+	u, err := uc.parseRequest(r)
+	if err != nil{
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Could not parse User object"))
+		return
+	}
+	u, err = models.AddUser(u)
+	if err != nil{
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	encodeResponseAsJSON(u, w)
+}
+
+func (uc *userController) parseRequest(r *http.Request) (models.User, error){
+	dec := json.NewDecoder(r.Body)
+	var u models.User
+	err := dec.Decode(&u)
+	if err != nil{
+		return models.User{}, err
+	}
+	return u, nil
 }
 
 func newUserController() *userController{
--- models/user.go
+++ models/user.go
@@ -2,6 +2,7 @@ package models
 
 import(
 	"errors"
+	"fmt"
 )
 
 // User is a model of the user
@@ -34,6 +35,31 @@ func AddUser(u User)  (User, error) {
 
 // GetUserByID returns user given ID
 func GetUserByID(id int) (User, error){
-	
-	return User{}, nil
+	for _, u := range users{
+		if u.ID == id{
+			return *u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", id)
+}
+// UpdateUser update an existing user
+func UpdateUser(u User) (User, error){
+	for i, user := range users{
+		if user.ID == u.ID {
+			users[i] = &u
+			return u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", u.ID)
+}
+
+// RemoveUserByID remove a user with a given ID
+func RemoveUserByID(id int) error{
+	for i, user := range users{
+		if user.ID == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("User with ID '%v' not found", id)
 }
